9577212fae6d1742bbc1144b2e4dfc3eceabcd78
Jovan <Jovan@DESKTOP-JTIACNO>
2021-05-12 18:02:50 +0200

    Role

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from app.api.api import api_router
 from app.settings.config import settings
 
-from starlette.middleware.cors import CORSMiddleware
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(
     title='P2PEnergy', openapi_url='/api/p2p/openapi.json'
