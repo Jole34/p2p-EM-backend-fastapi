35f7d24814a27364dfc1c24a8423a9f3c7b838c1
Jovan <Jovan@DESKTOP-JTIACNO>
2021-05-13 08:56:30 +0200

    test

--- app/schemas/billing.py
+++ app/schemas/billing.py
@@ -5,12 +5,10 @@ class Billing(BaseModel):
     city: str
     country: str
     zip_code: str
-    balance_id: int
 
 class BillingIn(BaseModel):
     address_line: str
     city: str
     country: str
     zip_code: str
-    balance_id: int
     user_id: int
